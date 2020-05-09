8b36373840fff446581c416dd6dfd67cfdf51408
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-05-08 17:12:25 -0700

    added get route for specific id

--- server.js
+++ server.js
@@ -9,11 +9,21 @@ const app = require('./lib/app');
 
 const PORT = process.env.PORT || 7890;
 
+// get route for all butterflies 
 app.get('/butterflies', async(req, res) => {
   const data = await client.query('SELECT * from butterflies');
 
   res.json(data.rows);
 });
+
+//get api route for one butterfly by particular id
+app.get('/butterflies/:id', async(req, res) => {
+  const id = req.params.id; // when using the :id we use params instead of search 
+  const data = await client.query(`SELECT * from butterflies WHERE id = ${id}`);
+
+  res.json(data.rows);
+});
+
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
